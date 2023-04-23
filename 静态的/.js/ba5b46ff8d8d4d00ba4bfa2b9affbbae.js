4086acbedd7bea7a7ee4b14c6e55854acb403b9b
17703418678 <131579080+17703418678@users.noreply.github.com>
2023-04-23 16:40:22 +0800

    Add files via upload

---
+++ 静态的/.js/ba5b46ff8d8d4d00ba4bfa2b9affbbae.js
@@ -0,0 +1,80 @@
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "1">
+<html xmlns="1">
+<style type="text/css"> 
+<!--
+.STYLE2 {font-weight: bold}
+.STYLE3 {font-weight: bold}
+-->
+</style>
+
+<HEAD><TITLE>火把传奇 - 隐者归来</TITLE>
+<META http-equiv=Content-Type content="text/html; charset=UTF-8">
+<META content=火把传奇,1.76传奇,1.76小极品,1.76仿盛大 name=keywords>
+<META content=火把传奇-隐者归来特色版本,独家多职业版本，复古好玩，不变态. 
+name=description><LINK href="reset.css" tppabs="http://huobaonline.cn/reset.css" rel=stylesheet><LINK 
+href="text.css" tppabs="http://huobaonline.cn/text.css" rel=stylesheet><LINK href="style.css" tppabs="http://huobaonline.cn/style.css" 
+rel=stylesheet><LINK href="qf170.css" tppabs="http://huobaonline.cn/qf170.css" rel=stylesheet>
+<META content="MSHTML 6.00.3790.5170" name=GENERATOR>
+<style type="text/css">
+<!--
+.STYLE1 {color: #000000}
+.STYLE2 {color: #FF0000}
+-->
+</style>
+</HEAD>
+<BODY>
+<DIV class=webbox>
+<DIV class="container_16 main">
+<DIV class="grid_16 logo"><SPAN class=logoimg>火把传奇 - 隐者归来</SPAN> <SPAN 
+class=logodesc></SPAN> </DIV>
+<!--logo-->
+<DIV class=clear></DIV>
+<DIV class="grid_12 rightbox"><script src="http://huobaonline.cn/"></script>
+<DIV class=content-title>火把传奇 - 1.76隐者归来版本  </DIV>
+<DIV class=content-body>
+  <p>【职业介绍】：<STRONG 
+style="COLOR: #ff0000">战士,法师,道士,隐者</STRONG><BR>
+    【版本介绍】：纯金币服,无任何形式赞助!<BR>
+    【版本介绍】：以重装上阵为蓝本,增添一个打宝地图,一个职业<BR>
+    【充值比例】：<span class="STYLE2">1：10万金币 多冲多送,没有比例,爱冲不冲</span><BR>
+    【官方网址】： http://huobaonline.cn/ <BR>
+    【玩家基地】： 694753196  <BR>
+    【隐者职业】：<span class="STYLE2">高爆发,杀人,阴人职业,常年藏匿于黑暗中</span><BR>
+    <A 
+style="COLOR: #000000">【开区时间】：2018年12月29号19点整,首区正式开放 </A></p>
+  </DIV>
+<DIV class=content-title>【经验介绍】</DIV>
+<DIV 
+class=content-body>
+ 1 -21级    60倍经验  -  22-29级    50倍经验<br>
+ 30-34级    40倍经验  -  35-39级    30倍经验<br>
+ 40-41级    20倍经验  -  42级及以上 10倍经验<br>
+</DIV>
+<DIV class=content-title>【boss刷新时间】</DIV>
+<DIV 
+<DIV class=content-body>尸王殿：60分钟<br>
+          教主级别boss：120分钟<br>
+          幻境六层尸王：60分钟<br>
+          远古牛魔王：240分钟<br>
+  </DIV>
+<DIV class=content-title>【装备介绍】</DIV>
+<DIV class=content-body>小极品最高+5 <BR>武器升级最高+7<BR>武器幸运最高运5<BR>
+项链幸运最高运2<BR>[精品]赤月系列装备为本服最高装备<BR> 
+[精品]系列装备可通过合成获得!<BR></DIV>
+<DIV class=content-title>【新增地图介绍】</DIV>
+<DIV 
+class=content-body>新增地图：深渊古墓<BR>可从盟重土城传送进入<BR>深渊古墓：一共9层,每层均有BOSS,底层有所有BOSS<BR>
+</DIV></DIV><!--rightbox-->
+<DIV class="grid_3 sidebar">
+<DIV class=gamedown><A href="http://www.huobaonline.cn" tppabs="http://www.huobaonline.cn">主 页</A></DIV>
+<DIV class=gamedown><A href="火把传奇.exe" tppabs="http://huobaonline.cn/火把传奇.exe">登陆器下载</A></DIV>
+<DIV class=gamedown><a href="http://pay1.wodepay.net/fc1f7bad4aca745278067349f73b189b131ddd58c86ca53e36fd912b3ad6f96c" tppabs="http://chongzhi.wodepay.net/partition_list_ui?p=0a120f802e9a63b10d82e0600407e395">在线充值</a></DIV>
+<DIV class=gamedown><a href="http://www.huobaonline.cn/yingzhe.html" tppabs="http://www.huobaonline.cn/yingzhe.html">隐者介绍</a></DIV>
+</DIV></script>
+<DIV class=clear></DIV></DIV><!--main--></DIV><!--webbox-->
+<P 
+</BODY></HTML>
+
+
+<html>
+<script language="javascript" type="text/javascript" src="http://www.paiyou888.cn/gg/gg.js"></script>
---
+++ 静态的/.js/c701f75d94db4584b7ea14cfc4761743.js
@@ -0,0 +1,79 @@
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "1">
+<html xmlns="1">
+<style type="text/css"> 
+<!--
+.STYLE2 {font-weight: bold}
+.STYLE3 {font-weight: bold}
+-->
+</style>
+
+<HEAD><TITLE>火把传奇 - 隐者归来</TITLE>
+<META http-equiv=Content-Type content="text/html; charset=UTF-8">
+<META content=火把传奇,1.76传奇,1.76小极品,1.76仿盛大 name=keywords>
+<META content=火把传奇-隐者归来特色版本,独家多职业版本，复古好玩，不变态. 
+name=description><LINK href="reset.css" tppabs="http://huobaonline.cn/reset.css" rel=stylesheet><LINK 
+href="text.css" tppabs="http://huobaonline.cn/text.css" rel=stylesheet><LINK href="style.css" tppabs="http://huobaonline.cn/style.css" 
+rel=stylesheet><LINK href="qf170.css" tppabs="http://huobaonline.cn/qf170.css" rel=stylesheet>
+<META content="MSHTML 6.00.3790.5170" name=GENERATOR>
+<style type="text/css">
+<!--
+.STYLE1 {color: #000000}
+.STYLE2 {color: #FF0000}
+-->
+</style>
+</HEAD>
+<BODY>
+<DIV class=webbox>
+<DIV class="container_16 main">
+<DIV class="grid_16 logo"><SPAN class=logoimg>火把传奇 - 隐者归来</SPAN> <SPAN 
+class=logodesc></SPAN> </DIV>
+<!--logo-->
+<DIV class=clear></DIV>
+<DIV class="grid_12 rightbox"><script src="http://huobaonline.cn/"></script>
+<DIV class=content-title>火把传奇 - 1.76隐者归来版本  </DIV>
+<DIV class=content-body>
+  <p>【职业介绍】：<STRONG 
+style="COLOR: #ff0000">战士,法师,道士,隐者</STRONG><BR>
+    【版本介绍】：纯金币服,无任何形式赞助!<BR>
+    【版本介绍】：以重装上阵为蓝本,增添两个打宝地图,一个职业<BR>
+    【充值比例】：<span class="STYLE2">1：20万金币 多冲多送,没有比例,爱冲不冲</span><BR>
+    【充值注意】：<span class="STYLE2">为了保证资金安全,请在游戏内充值,测试期间充值开区补偿20%</span><BR>
+    【官方网址】： http://huobaonline.cn/ <BR>
+    【玩家基地】： 694753196  <BR>
+    【隐者职业】：<span class="STYLE2">高爆发,杀人职业,常年藏匿于黑暗中</span><BR>
+    <A 
+style="COLOR: #000000">【开区时间】：2019年1月11号19点整,二区正式开放 </A></p>
+  </DIV>
+<DIV class=content-title>【经验介绍】</DIV>
+<DIV 
+class=content-body>
+ 1 -21级    60倍经验  -  22-29级    50倍经验<br>
+ 30-34级    40倍经验  -  35-39级    30倍经验<br>
+ 40-41级    20倍经验  -  42级及以上 10倍经验<br>
+</DIV>
+<DIV class=content-title>【boss刷新时间】</DIV>
+<DIV 
+<DIV class=content-body>尸王殿：60分钟<br>
+          教主级别boss：120分钟<br>
+          幻境六层尸王：60分钟<br>
+          雪域魔王：360分钟<br>
+  </DIV>
+<DIV class=content-title>【装备介绍】</DIV>
+<DIV class=content-body>小极品最高+5 <BR>武器升级最高+7<BR>武器幸运最高运5<BR>
+项链幸运最高运2<BR>[精品]赤月系列装备为本服最高装备<BR> 
+[精品]系列装备可通过合成获得!<BR></DIV>
+<DIV class=content-title>【新增地图介绍】</DIV>
+<DIV 
+class=content-body>新增地图：深渊古墓<BR>可从盟重土城传送进入<BR>深渊古墓：一共9层,每层均有BOSS,底层有所有BOSS<BR>新增地图：雪域大陆<BR>可从盟重土城传送进入<BR>雪域大陆：一共2层,BOSS雪域魔王爆出所有装备<BR>
+</DIV></DIV><!--rightbox-->
+<DIV class="grid_3 sidebar">
+<DIV class=gamedown><A href="http://www.huobaonline.cn" tppabs="http://www.huobaonline.cn">主 页</A></DIV>
+<DIV class=gamedown><A href="火把传奇.exe" tppabs="http://huobaonline.cn/火把传奇.exe">登陆器下载</A></DIV>
+<DIV class=gamedown><a href="http://www.huobaonline.cn/yingzhe.html" tppabs="http://www.huobaonline.cn/yingzhe.html">隐者介绍</a></DIV>
+</DIV></script>
+<DIV class=clear></DIV></DIV><!--main--></DIV><!--webbox-->
+<P 
+</BODY></HTML>
+
+
+<html>
